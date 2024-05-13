7c3497d19b9aa3ce91794cc171f7741595130cff
Mickael Turquais <mickael.turquais@laposte.net>
2024-05-13 18:08:38 +0200

    feat: fichier env

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_BASE_URL_SERVER,
 });
 
 export default apiRequest;
